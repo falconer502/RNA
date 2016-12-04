a74e2d7d0db3716113d95c1c10d15e61865ee77f
falconer502 <wangyongqiang502@126.com>
2016-12-04 21:58:35 +0800

    Update rna-seq_flow.py

--- rna-seq_flow.py
+++ rna-seq_flow.py
@@ -1,6 +1,6 @@
 import subprocess
 import os,shutil,getopt,sys
-
+global datalist
 def write_log(log):
 	f=open('log','a')
 	print(log,file = f)
@@ -10,9 +10,9 @@ def exec(*arg) :   #write logfile
 	out=subprocess.Popen(arg[0],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)#arg[0] command ,arg[1] comment
 	write_log(arg[1])
 	while out.poll()==None:       #check whether the process is commplete, if  have completed return 0
-		write_log(out.stderr.read().decode('utf-8'))
-		print("poll code is"+str(out.poll()))
-	print("#######################################")
+		write_log(out.stdout.read().decode('utf-8'))
+		write_log("poll code is"+str(out.poll()))
+	write_log("#######################################")
 
 def mkdir(dirname):
 	dirname=dirname.strip()
@@ -34,7 +34,7 @@ def extract_es(gtf):                   #create splicesites fileT
 def build_index(genome,gtf):
 	extract_es(gtf)
 	mkdir('index')
-	cmd='hisat2-build -f %s --ss splicesites.txt --exon exons.txt -p 7 ./index/genome.index' %genome
+	cmd='hisat2-build -f %s --ss splicesites.txt --exon exons.txt -p 20 ./index/genome.index' %genome
 	exec(cmd,'build index with hisat2')
 
 #exec("ls -s","pirnt dir....")
@@ -46,36 +46,35 @@ def align_genome(genome,fa_path):   #align genome,input path of data files
 			file1,file2=os.listdir(fa_path+i)
 			file1=fa_path+i+'/'+file1
 			file2=fa_path+i+'/'+file2
-			cmd='hisat2 -q -x ./index/genome.index -1 %s -2 %s -S ./temp/%s.sam -t'%(file1,file2,i)
-			exec(cmd ,"%s algin genome  complete with hisat2"%i) #execute commad
+			cmd='hisat2 -q -x ./index/genome.index -1 %s -2 %s -S ./temp/%s.sam -p 20 -t'%(file1,file2,i)
+			exec(cmd ,"%s is algining against genome  using hisat2"%i) #execute commad
 def stringtie(gtf,filepath):
-	for i in os.listdir(filepath):
-		if '.sam' in i: #i==t01.sam t02.sam t01.sam.sorted.bam
-			name=i  #name==t01.sam.sorted.bam
-			if 'merged.gtf' in gtf :
-				if 'sorted.bam' in i:
-					name=i.split('.')[0]
-					print(i)
-					exec('stringtie ./temp/%s -b ./results/%s -e -G ./merge/merged.gtf -C ./results/%s.cov_ref.gtf -p 7 -o ./results/%s.stringtie.out.gtf'%(i,name,name,name),'transcript assembly and qualntification for RNA-SEQ using stingtie ')
-				else:
-					continue
-					# stringite after emerging  gtf  #t01.sam.sorted.bam  -b t01.sam -G ./temp/merged.gtf -c t01.cov_ref.gtf -o t01.sam.stringtie.out.gtf
-			else:	
-				exec('samtools view -Su ./temp/%s | samtools sort > ./temp/%s.sorted.bam'%(name,name),'converted to bam format using samtools')
-				exec('stringtie ./temp/%s.sorted.bam -G %s -p 7 -o ./temp/stringtie/%s.stringtie.out.gtf'%(name,gtf,name),'transcript assembly and qualntification for RNA-SEQ using stingtie ')
+	datalist_tmp=[i+'.sam' for i in datalist]
+	if set(datalist_tmp).issubset(set(os.listdir(filepath))) and 'merged.gtf' in gtf :
+		for i in os.listdir(filepath):
+			name=i.split('.')[0]
+			print(i)
+			exec('stringtie ./temp/%s -b ./results/%s -e -G ./merge/merged.gtf -C ./results_gtf/%s.cov_ref.gtf -p 20 -o ./results_gtf/%s.stringtie.out.gtf'%(i,name,name,name),'transcript assembly and qualntification for RNA-SEQ using stingtie ')
+			# stringite after emerging  gtf  #t01.sam.sorted.bam  -b t01.sam -G ./temp/merged.gtf -c t01.cov_ref.gtf -o t01.sam.stringtie.out.gtf
+	else:	
+		for i in os.listdir(filepath):
+			name=i.split('.')[0]
+			exec('samtools view -Su ./temp/%s | samtools sort > ./temp/%s.sorted.bam'%(name,name),'converted %s to bam format using samtools'%(name))
+			exec('stringtie ./temp/%s.sorted.bam -G %s -p 20 -o ./temp/stringtie/%s.stringtie.out.gtf'%(name,gtf,name),'%s transcript assembly and qualntification for RNA-SEQ using stingtie'%(name))
 			#t01.sam.sorted.bam
 def merge(filepath,gtf):
 	list=os.listdir(filepath)
 	list=[filepath+i for i in list]
 	listfile=' '.join(list)
-	print(listfile)
+	write_log(listfile)
 	exec('stringtie --merge %s -G %s -o ./merge/merged.gtf'%(listfile,gtf),'merge all gtf.......')
+	exec('rm %s'%(listfile))
 #def ballgown():
 
 
 
 def usuage():
-	print('''
+	write_log('''
 		g: input genome file
 		f: input cleandata file path
 		t: input gtf file''')
@@ -96,32 +95,33 @@ if __name__=="__main__":
 				print('%-20s: %-10s'%('gtf file is',value))
 			if name in ('-f','--filepath'):
 				filepath=value
+				datalist=os.listdir(filepath)
 				print('%-20s: %-10s'%('fasta filepath are',value))
 			if name in ('-h','-help'):
 				usuage()
 				sys.exit()
-		print("===============opt=====================")
-		print('start analysing..............')	
+		write_log("===============opt=====================")
+		write_log('start analysing..............')	
 		
 	except getopt.GetoptError:
-		print("getopt error")
+		write_log("getopt error")
 		usuage()
 		sys.exit()
 	
 	
-	print("setp1: creat temp directory.....")
+	write_log("setp1: creat temp directory.....")
 	mkdir('temp')
 	mkdir('./temp/stringtie')
 	mkdir('merge')
 	mkdir('results')    #create a temporary  folder
-	print('setp2: build index..............')
+	write_log('setp2: build index..............')
 	build_index(genome,gtf)
-	print('step3: algin genome.............')
+	write_log('step3: algin genome.............')
 	align_genome(genome,filepath) # align genome and remove the result to temp)
 
-	print('step4:stringtie.................')
+	write_log('step4:stringtie.................')
 	stringtie(gtf,'./temp') 
-	print('step5:merging...................')
+	write_log('step5:merging...................')
 	merge('./temp/stringtie/',gtf)
-	print('step6:stringtie after merged....')
+	write_log('step6:stringtie after merged....')
 	stringtie('./merge/merged.gtf','./temp')
